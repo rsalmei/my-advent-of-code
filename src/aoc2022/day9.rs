178c345fe91ffb4648c5e30e75bd033ce6eb8c8e
Rogério Almeida <rsalmei@gmail.com>
2026-04-02 01:39:35 -0300

    remove repetitions and just use number of steps

--- src/aoc2022/day9.rs
+++ src/aoc2022/day9.rs
@@ -1,40 +1,37 @@
 use crate::Input;
 use std::collections::HashSet;
-use std::iter;
 
 pub fn run(input: Input) {
-    let data = input
-        .lines()
-        .flat_map(|s| {
-            let (direction, steps) = s.split_once(' ').unwrap();
-            let steps = steps.parse::<usize>().unwrap();
-            iter::repeat(direction).take(steps)
-        })
-        .collect::<Vec<_>>();
+    let data = input.map_lines(|s| {
+        let (direction, steps) = s.split_once(' ').unwrap();
+        (direction.as_bytes()[0], steps.parse().unwrap())
+    });
 
     // part one.
-    fn sim(data: &[&str], tail_size: usize) -> usize {
+    fn sim(data: &[(u8, usize)], tail_size: usize) -> usize {
         let mut head = (0i32, 0i32);
         let mut rope = vec![(0, 0); tail_size];
         let mut visited = HashSet::new();
-        for &s in data {
-            match s {
-                "U" => head.1 += 1,
-                "D" => head.1 -= 1,
-                "L" => head.0 -= 1,
-                "R" => head.0 += 1,
-                _ => unreachable!(),
-            }
-            rope.iter_mut().fold(head, |head, tail| {
-                if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
-                    tail.0 += (head.0 - tail.0).clamp(-1, 1);
-                    tail.1 += (head.1 - tail.1).clamp(-1, 1);
+        data.iter().for_each(|&(s, steps)| {
+            for _ in 0..steps {
+                match s {
+                    b'U' => head.1 += 1,
+                    b'D' => head.1 -= 1,
+                    b'L' => head.0 -= 1,
+                    b'R' => head.0 += 1,
+                    _ => unreachable!(),
                 }
-                *tail
-            });
-            visited.insert(rope[tail_size - 1]);
-            // println!("{s} -> H {head:?}  T {tail:?} ({})", visited.len());
-        }
+                rope.iter_mut().fold(head, |head, tail| {
+                    if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
+                        tail.0 += (head.0 - tail.0).clamp(-1, 1);
+                        tail.1 += (head.1 - tail.1).clamp(-1, 1);
+                    }
+                    *tail
+                });
+                visited.insert(rope[tail_size - 1]);
+                // println!("{s} -> H {head:?}  T {tail:?} ({})", visited.len());
+            }
+        });
         visited.len()
     }
     println!("{}", sim(&data, 1));
