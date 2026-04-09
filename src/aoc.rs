fed3e6a9cdb468c4b57d1bb64a0c851c04eb395d
Rogério Almeida <rsalmei@gmail.com>
2026-04-09 13:19:06 -0300

    trick to use the macro before it is defined in the same file

--- src/aoc.rs
+++ src/aoc.rs
@@ -1,12 +1,15 @@
-use crate::input::Input;
-use std::collections::BTreeMap;
+aoc! {
+    2018: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    2022: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    2025: [1, 2, 3],
+}
 
 /// Register Advent of Code solutions.
 ///
 /// Generate module declarations and a [collect_years] function that returns a nested
 /// `BTreeMap` mapping years and days to their respective `run` functions.
 #[macro_export]
-macro_rules! aoc {
+macro_rules! _aoc {
     ($($year:literal: [$($day:literal),* $(,)?]),* $(,)?) => {
         paste::paste! {
             $(
@@ -15,6 +18,8 @@ macro_rules! aoc {
                 }
             )*
 
+            use crate::input::Input;
+            use std::collections::BTreeMap;
             pub fn collect_years() -> BTreeMap<u16, BTreeMap<u8, fn(Input)>> {
                 BTreeMap::from([
                     $(
@@ -25,9 +30,4 @@ macro_rules! aoc {
         }
     };
 }
-
-aoc! {
-    2018: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    2022: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    2025: [1, 2, 3],
-}
+use _aoc as aoc;
